f72eec03163f38adf2bae62a29fb3c6ad6121a23
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2022-07-14 22:41:09 +0200

    fix imports

--- src/test/exports.test.ts
+++ src/test/exports.test.ts
@@ -1,4 +1,8 @@
-import { parse, File, MappedNode, Range, Location, Position } from '../index'
+import {
+  parse, File, MappedNode, MappedArray, MappedObject, MappedPrimitive,
+  isNumberNode, isBooleanNode, isStringNode, isArrayNode, isObjectNode,
+  Range, Location, Position
+} from '../index'
 
 
 test('everything is properly exported.', () => {
@@ -8,5 +12,15 @@ test('everything is properly exported.', () => {
   expect(<Range>{}).not.toBe(undefined)
   expect(<Location>{}).not.toBe(undefined)
   expect(<Position>{}).not.toBe(undefined)
+  expect(<MappedArray>{}).not.toBe(undefined)
+  expect(<MappedObject>{}).not.toBe(undefined)
+  expect(<MappedPrimitive<number>>{}).not.toBe(undefined)
+  expect(<MappedPrimitive<boolean>>{}).not.toBe(undefined)
+  expect(<MappedPrimitive<string>>{}).not.toBe(undefined)
+  expect(isNumberNode).not.toBe(undefined)
+  expect(isBooleanNode).not.toBe(undefined)
+  expect(isStringNode).not.toBe(undefined)
+  expect(isArrayNode).not.toBe(undefined)
+  expect(isObjectNode).not.toBe(undefined)
 })
 
